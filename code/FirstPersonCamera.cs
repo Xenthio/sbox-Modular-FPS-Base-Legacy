13b013b515e362ba19b6e211f53c0c68481cf55a
Xenthio <thegooster88@gmail.com>
2023-02-02 15:13:54 +1100

    Initial commit.

---
+++ code/FirstPersonCamera.cs
@@ -0,0 +1,36 @@
+﻿using Sandbox;
+
+namespace MyGame;
+
+public class FirstPersonCamera : SimulatedComponent
+{
+	public override void FrameSimulate( IClient cl )
+	{
+
+		var pl = Entity as Player;
+		// Update rotation every frame, to keep things smooth
+		Entity.Rotation = pl.ViewAngles.ToRotation();
+
+		Camera.Position = Entity.Position + new Vector3( 0, 0, 64 );
+		Camera.Rotation = Entity.Rotation;
+
+		// Set field of view to whatever the user chose in options
+		Camera.FieldOfView = Screen.CreateVerticalFieldOfView( Game.Preferences.FieldOfView );
+
+		// Set the first person viewer to this, so it won't render our model
+		Camera.FirstPersonViewer = Entity;
+	}
+	public override void BuildInput()
+	{
+		var pl = Entity as Player;
+		var look = Input.AnalogLook;
+		if ( pl.ViewAngles.pitch > 90f || pl.ViewAngles.pitch < -90f )
+		{
+			look = look.WithYaw( look.yaw * -1f );
+		}
+		var viewAngles = pl.ViewAngles;
+		viewAngles += look;
+		viewAngles.pitch = viewAngles.pitch.Clamp( -89f, 89f );
+		pl.ViewAngles = viewAngles.Normal;
+	}
+}
---
+++ code/Player.cs
@@ -0,0 +1,60 @@
+﻿using Sandbox;
+namespace MyGame;
+
+partial class Player : AnimatedEntity
+{
+	/// <summary>
+	/// Called when the entity is first created 
+	/// </summary>
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		//
+		// Use a watermelon model
+		//
+		SetModel( "models/sbox_props/watermelon/watermelon.vmdl" );
+		Components.Add( new WalkController() );
+		Components.Add( new FirstPersonCamera() );
+		EnableDrawing = true;
+		EnableHideInFirstPerson = true;
+		EnableShadowInFirstPerson = true;
+	}
+
+	// An example BuildInput method within a player's Pawn class.
+	[ClientInput] public Vector3 InputDirection { get; set; }
+	[ClientInput] public Angles ViewAngles { get; set; }
+
+	public override void BuildInput()
+	{
+		foreach ( var i in Components.GetAll<SimulatedComponent>() )
+		{
+			i.BuildInput();
+		}
+	}
+
+	/// <summary>
+	/// Called every tick, clientside and serverside.
+	/// </summary>
+	public override void Simulate( IClient cl )
+	{
+		base.Simulate( cl );
+
+		foreach ( var i in Components.GetAll<SimulatedComponent>() )
+		{
+			i.Simulate( cl );
+		}
+	}
+
+	/// <summary>
+	/// Called every frame on the client
+	/// </summary>
+	public override void FrameSimulate( IClient cl )
+	{
+		base.FrameSimulate( cl );
+		foreach ( var i in Components.GetAll<SimulatedComponent>() )
+		{
+			i.FrameSimulate( cl );
+		}
+	}
+}
