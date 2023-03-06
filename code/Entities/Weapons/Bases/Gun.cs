c1de66c83f798e2d77025292983acaa5d753b1aa
Xenthio <thegooster88@gmail.com>
2023-03-06 19:49:51 +1100

    update spread math

--- code/Entities/Weapons/Bases/Gun.cs
+++ code/Entities/Weapons/Bases/Gun.cs
@@ -23,8 +23,9 @@ public partial class Gun : Weapon
 	public virtual void ShootBullet( float damage = 0, float spread = 0, float force = 0 )
 	{
 		Game.SetRandomSeed( Time.Tick );
-		var spreadvec = Vector3.Random * spread;
-		var tr = Trace.Ray( Owner.AimRay.Position, (spreadvec + Owner.AimRay.Forward) * 65565 ).UseHitboxes().Ignore( Owner ).Run();
+		var forward = Owner.AimRay.Forward;
+		forward += (Vector3.Random + Vector3.Random + Vector3.Random + Vector3.Random) * spread * 0.25f;
+		var tr = Trace.Ray( Owner.AimRay.Position, forward * 65565 ).UseHitboxes().Ignore( Owner ).Run();
 		if ( tr.Hit )
 		{
 			tr.Surface.DoBulletImpact( tr );
