bfbc6c5aa1831e06234177cd6d2467462a73de89
Xenthio <thegooster88@gmail.com>
2023-03-08 10:42:54 +1100

    Simple Grenade

---
+++ code/Entities/Weapons/Bases/Throwable.cs
@@ -0,0 +1,33 @@
+﻿using Sandbox;
+
+namespace MyGame;
+public partial class Throwable : Carriable
+{
+	public override void Simulate( IClient cl )
+	{
+		if ( Owner is not Player ) return;
+		if ( WillThrow() )
+		{
+			using ( LagCompensation() )
+			{
+				Throw();
+			}
+			if ( Game.IsServer )
+			{
+				if ( Owner is Player ply )
+				{
+					ply.Inventory?.Items.Remove( this );
+				}
+				Delete();
+			}
+		}
+	}
+	public virtual bool WillThrow()
+	{
+		return Input.Released( InputButton.PrimaryAttack );
+	}
+	public virtual void Throw()
+	{
+
+	}
+}
