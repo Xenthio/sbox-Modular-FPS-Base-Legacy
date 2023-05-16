4180ef20819719d3629df8c76d81414d14fa2368
Xenthio <thegooster88@gmail.com>
2023-05-16 10:00:56 +1000

    fix being able to clip out of bounds on an upwards slope when up against the ceiling

--- code/Player/Movement/WalkController.cs
+++ code/Player/Movement/WalkController.cs
@@ -532,7 +532,7 @@ public partial class WalkController : MovementComponent
 	public virtual void TryUnDuck()
 	{
 		UpdateBBox( -1 );
-		var pm = TraceBBox( Entity.Position, Entity.Position, DuckHeight );
+		var pm = TraceBBox( Entity.Position, Entity.Position, DuckHeight - 1 );
 		if ( pm.StartedSolid )
 		{
 			UpdateBBox();
@@ -550,7 +550,7 @@ public partial class WalkController : MovementComponent
 			return;
 
 		var test = Entity.Position;
-		for ( int i = 0; i < 36; i++ )
+		for ( int i = 0; i < (DuckHeight - 1); i++ )
 		{
 			var org = Entity.Position;
 			org.z += direction;
